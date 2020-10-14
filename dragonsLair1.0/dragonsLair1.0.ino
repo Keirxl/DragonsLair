679f6c130e441032e2f9fd4d5cad46e86b4e05d2
Keir Williams <keirsebastien@gmail.com>
2020-10-14 10:38:55 -0400

    Create dragonsLair1.0.ino

---
+++ dragonsLair1.0/dragonsLair1.0.ino
@@ -0,0 +1,18 @@
+#define FIELD_COLOR makeColorHSB(200,20,100)
+
+//[A][B][C][D][E][F]
+
+enum blinkType {FIELD,PLAYER}; //[A]
+byte blinkType;
+enum attackSignal {INERT,FIRE,POISON,VOID,RESOLVE};
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
