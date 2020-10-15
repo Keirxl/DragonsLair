096a55f7d5992a60e4c9c5e7d3c34c0e5a1f6937
Keir Williams <keirsebastien@gmail.com>
2020-10-15 11:31:33 -0400

    Update dragonsLair1.0.ino

--- dragonsLair1.0/dragonsLair1.0.ino
+++ dragonsLair1.0/dragonsLair1.0.ino
@@ -1,7 +1,12 @@
-#define FIELD_COLOR makeColorHSB(200,60,60)
-#define FIRE_ATTACK_LENGTH 1500
+#define FIELD_COLOR makeColorHSB(200,60,100)
+#define FIRE_DURATION 1500
+#define POISON_DURATION 3000
+#define VOID_DURATION 5000
 #define FIRE_DELAY_TIME 500
+#define VOID_DELAY_TIME 500
 #define POISON_DELAY_TIME 1000
+#define DRAGON_WAIT_TIME 7000
+#define DRAGON_ATTACK_DURATION 1000
 //[A][B][C][D][E][F]
 
 enum blinkType {FIELD,PLAYER}; //[A]
@@ -14,13 +19,14 @@ byte sendData;
 
 bool isDragon=false;
 
-Timer fireAttackLingerTimer;
-Timer fireDelayTimer;
-Timer poisonDelayTimer;
+Timer delayTimer;
+Timer attackDurationTimer;
+Timer dragonWaitTimer;
+Timer dragonAttackTimer;
 
 void setup() {
   // put your setup code here, to run once:
-
+  randomize();
 }
 
 void loop() {
@@ -48,16 +54,39 @@ void loop() {
     byte clicks=buttonClickCount();
     if(clicks==3){
       isDragon=true;
+      dragonWaitTimer.set(DRAGON_WAIT_TIME);
     }
   }
 
+  //dragonAttacks
+  if(isDragon){
+     if(dragonWaitTimer.isExpired()){
+        if(noNeighborsAttacking){
+          dragonAttackTimer.set(DRAGON_ATTACK_DURATION);
+          byte whichAttack=random(99);
+          if(whichAttack<33){
+            attackSignal=FIRE;
+          }else if(33<=whichAttack<66){
+            attackSignal=POISON;
+          }else{
+            attackSignal=VOID;
+          }
+        }
+      dragonWaitTimer.set(DRAGON_WAIT_TIME);
+     }
+  }
+
+
    sendData = (blinkType<<5) + (attackSignal<<2);
    setValueSentOnAllFaces(sendData);
+
+   displayLoop();
   
 }
 
 void inertLoop(){
-  
+
+  //recieves attacks and delays sending them until it's time 
   FOREACH_FACE(f) {
     if (!isValueReceivedOnFaceExpired(f)) {//a neighbor!
       if (getAttackSignal(getLastValueReceivedOnFace(f)) == FIRE || POISON || VOID) {
@@ -65,22 +94,35 @@ void inertLoop(){
           hiddenAttackSignal=getAttackSignal(getLastValueReceivedOnFace(f));
           if(hiddenAttackSignal==FIRE){
             //set timer and display fire but don't BE fire until timer is up
-            fireDelayTimer.set(FIRE_DELAY_TIME);
-            //set Timer for how long to be fire? or do in fire loop
+            delayTimer.set(FIRE_DELAY_TIME);
           }else if(hiddenAttackSignal==POISON){
             //setTimer for poisionDisplay but don't BE poison until timer is out
-            poisonDelayTimer.set(POISON_DELAY_TIME);
-            //set timer for how long to be poision.
-            
+            delayTimer.set(POISON_DELAY_TIME);
           }else if(hiddenAttackSignal==VOID){
             //set timer and display void, but don't BE void until timer is out
-            fireDelayTimer.set(FIRE_DELAY_TIME);
-            //set Timer for how long to be VOID
+            delayTimer.set(VOID_DELAY_TIME);
           }
         }
       }
     }
   }
+
+//When the delay is over, it's time to send the signal and set the duration of the attack
+ if(delayTimer.isExpired()){
+   attackSignal=hiddenAttackSignal;
+   switch(hiddenAttackSignal){
+      case FIRE:
+        attackDurationTimer.set(FIRE_DURATION);
+        break;
+      case POISON:
+        attackDurationTimer.set(POISON_DURATION);
+        break;
+      case VOID:
+        attackDurationTimer.set(VOID_DURATION);
+        break;
+   }
+   hiddenAttackSignal=INERT;
+ }
 
 
 
@@ -89,24 +131,50 @@ void inertLoop(){
 }
 
 void fireLoop(){
-  
+  if(isDragon){
+    if(dragonAttackTimer.isExpired()){
+      attackSignal=INERT;
+    }
+  }else{
+    if(attackDurationTimer.isExpired()){
+      attackSignal=RESOLVE;
+    }
+  }
 }
 
 void poisonLoop(){
-  
+  if(isDragon){
+    if(dragonAttackTimer.isExpired()){
+      attackSignal=INERT;
+    }
+  }else{
+    if(attackDurationTimer.isExpired()){
+      attackSignal=RESOLVE;
+    }
+  }
 }
 
 void voidLoop(){
-  
+  if(isDragon){
+    if(dragonAttackTimer.isExpired()){
+      attackSignal=INERT;
+    }
+  }else{
+    if(attackDurationTimer.isExpired()){
+      attackSignal=RESOLVE;
+    }
+  }
 }
 
 void resolveLoop(){
   attackSignal=INERT;
-  
-  FOREACH_FACE(f) {
-    if (!isValueReceivedOnFaceExpired(f)) {//a neighbor!
-      if (getAttackSignal(getLastValueReceivedOnFace(f)) == FIRE || POISON || VOID) {//This neighbor isn't in RESOLVE. Stay in RESOLVE
-        attackSignal = RESOLVE;
+
+  if(!isDragon){
+    FOREACH_FACE(f) {
+      if (!isValueReceivedOnFaceExpired(f)) {//a neighbor!
+        if (getAttackSignal(getLastValueReceivedOnFace(f)) == FIRE || POISON || VOID) {//This neighbor isn't in RESOLVE. Stay in RESOLVE
+          attackSignal = RESOLVE;
+        }
       }
     }
   }
@@ -115,8 +183,43 @@ void resolveLoop(){
 void displayLoop(){
   if(isDragon){
     setColor(MAGENTA);
+  }else{
+    switch(attackSignal){
+      case FIRE:
+        setColor(ORANGE);
+        break;
+      case POISON:
+        setColor(GREEN);
+        break;
+      case VOID:
+        setColor(OFF);
+        break;
+      case INERT:
+        setColor(FIELD_COLOR);
+        break;
+      case RESOLVE:
+        setColor(BLUE);
+        break;
+    }
   }
   
+  
+}
+
+bool noNeighborsAttacking(){
+  byte neighborsAttacking=0;
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f)) {//a neighbor!
+      if (getAttackSignal(getLastValueReceivedOnFace(f)) == FIRE || POISON || VOID) {
+        neighborsAttacking++;
+      }
+    }
+  }
+  if(neighborsAttacking==0){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 byte getBlinkType(byte data){
