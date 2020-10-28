9a7863d8bd99741713eadfee91c5606b340cea3d
Keir Williams <keirsebastien@gmail.com>
2020-10-28 17:01:10 -0400

    Update Dragon_sLairScore.ino

--- dragonsLair1.0/Dragon_sLairScore/Dragon_sLairScore.ino
+++ dragonsLair1.0/Dragon_sLairScore/Dragon_sLairScore.ino
@@ -2,14 +2,17 @@
 #define RANDOMSCORE 800
 #define BLINK_WAIT_TIME 700
 #define BLINK_PAUSE_TIME 3000
+#define BLINK_SHOW_TIME 200
 
 int playerScore = 0;
 byte hundreds;
 byte tens;
 byte ones;
+byte whichScore=10;
 
 Timer treasureSpawnTimer;
 Timer dragonWaitTimer;
+Timer attackDurationTimer;
 
 void setup() {
   randomize();
@@ -18,7 +21,8 @@ void setup() {
 void loop() {
   
   if(buttonDoubleClicked()){
-    playerScore=random(RANDOMSCORE);
+    //playerScore=random(RANDOMSCORE);
+    playerScore=256;
     hundreds =(playerScore-(playerScore%100))/100;
     tens = ((playerScore%100)-(playerScore%10))/10;
     ones = (playerScore%10);
@@ -29,35 +33,47 @@ void loop() {
 }
 
 void scoreDisplay(){
-        setColorOnFace(dim(YELLOW,120),0);
-        setColorOnFace(dim(YELLOW,120),1);
-        setColorOnFace(dim(GREEN,120),2);
-        setColorOnFace(dim(GREEN,120),3);
-        setColorOnFace(dim(RED,120),4);
-        setColorOnFace(dim(RED,120),5);
+
 
   if(dragonWaitTimer.isExpired()){
     if(treasureSpawnTimer.isExpired()){
       if(hundreds>0){
-        setColorOnFace(YELLOW,0);
-        setColorOnFace(YELLOW,1);
+        whichScore=0;
         hundreds--;
       }else if(tens>0){
-        setColorOnFace(GREEN,2);
-        setColorOnFace(GREEN,3);
+        whichScore=2;
         tens--;
       }else if(ones>0){
-        setColorOnFace(RED,4);
-        setColorOnFace(RED,5);
+        whichScore=4;
         ones--;
       }else{
         dragonWaitTimer.set(BLINK_PAUSE_TIME);
-        hundreds =(playerScore-(playerScore%100))/100;
+        hundreds = (playerScore-(playerScore%100))/100;
         tens = ((playerScore%100)-(playerScore%10))/10;
         ones = (playerScore%10);
       }
+      attackDurationTimer.set(BLINK_SHOW_TIME);
       treasureSpawnTimer.set(BLINK_WAIT_TIME);
     }
+  }
+   setColorOnFace(dim(YELLOW,120),0);
+   setColorOnFace(dim(YELLOW,120),1);
+   setColorOnFace(dim(GREEN,120),2);
+   setColorOnFace(dim(GREEN,120),3);
+   setColorOnFace(dim(RED,120),4);
+   setColorOnFace(dim(RED,120),5);
+
+  if(!attackDurationTimer.isExpired()){
+      if(whichScore==0){
+        setColorOnFace(YELLOW,0);
+        setColorOnFace(YELLOW,1);
+      }else if(whichScore==2){
+         setColorOnFace(GREEN,2);
+         setColorOnFace(GREEN,3);
+      }else if(whichScore==4){
+        setColorOnFace(RED,4);
+        setColorOnFace(RED,5);
+      }
   }
   
 }
